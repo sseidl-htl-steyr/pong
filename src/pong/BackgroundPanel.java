2d372323225af151976cd8dbcd376609c144ee81
Bernd Berger <Bernd Berger@192.168.52.91>
2017-12-11 10:11:32 +0100

    BackgroundPanel

---
+++ src/pong/BackgroundPanel.java
@@ -0,0 +1,24 @@
+package pong;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+public class BackgroundPanel extends JPanel
+{
+	Image bgImage;
+	
+	BackgroundPanel(Image bgImage)
+	{
+		this.bgImage = bgImage;
+	}
+	
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		 g.drawImage(bgImage, 0, 0, null);
+	}
+}
