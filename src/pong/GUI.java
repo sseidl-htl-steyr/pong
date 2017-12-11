1b6b175acf0fc1eeee96bdef2930a1ec233d3062
Bernd Berger <Bernd Berger@192.168.52.91>
2017-12-11 10:09:16 +0100

    Startbildschirm, Buttons gehoeren noch veraendert!

--- src/pong/GUI.java
+++ src/pong/GUI.java
@@ -4,12 +4,15 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 
 public class GUI extends JFrame implements ActionListener
 {
@@ -17,14 +20,13 @@ public class GUI extends JFrame implements ActionListener
 	private JPanel mainMenu;
 	private JPanel canvas;
 
-	
-	
-	//Menübuttons
+	// Menübuttons
+	private JTextPane title;
 	private JButton singleplayer;
 	private JButton multiplayer;
 	private JButton settings;
 	private JButton exit;
-	
+
 	public GUI()
 	{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -32,46 +34,76 @@ public class GUI extends JFrame implements ActionListener
 		setSize(800, 500);
 		setResizable(false);
 
+		setIconImage(loadImage("Pong_Icon.png"));
+//		setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("Pong_Icon.png")));
+		
 		Container contentPane = getContentPane();
 		cl = new CardLayout();
 		contentPane.setLayout(cl);
-		
-		mainMenu = new JPanel(new GridLayout(4, 1));
-		mainMenu.setBackground(Color.BLACK);
-		
+
+		mainMenu = new BackgroundPanel(loadImage("Pong.png"));
+		mainMenu.setLayout(new GridLayout(5, 1));
+//		mainMenu = new JPanel(new GridLayout(5, 1));
+//		mainMenu.setBackground(Color.GREEN);
+
 		canvas = new JPanel();
 		canvas.setBackground(Color.WHITE);
-		
+
 		contentPane.add(mainMenu);
 		contentPane.add(canvas);
-		
+
 		cl.addLayoutComponent(mainMenu, "main");
 		cl.addLayoutComponent(canvas, "canvas");
-		
+
 		cl.show(contentPane, "main");
+
 		
-		
+		title = new JTextPane();
+		title.setText("Pong");
+		title.setEditable(false);
+		mainMenu.add(title);
+
 		singleplayer = new JButton("Singleplayer");
+		singleplayer.setOpaque(true);
+		singleplayer.setContentAreaFilled(false);
+		singleplayer.setBorderPainted(false);
 		mainMenu.add(singleplayer);
-		
+
 		multiplayer = new JButton("Multiplayer");
+		multiplayer.setOpaque(false);
+		multiplayer.setContentAreaFilled(false);
+		multiplayer.setBorderPainted(false);
 		mainMenu.add(multiplayer);
-		
+
 		settings = new JButton("Settings");
+		settings.setOpaque(false);
+		settings.setContentAreaFilled(false);
+		settings.setBorderPainted(false);
 		mainMenu.add(settings);
-		
+
 		exit = new JButton("Exit");
+		exit.setOpaque(false);
+		exit.setContentAreaFilled(false);
+		exit.setBorderPainted(false);
 		mainMenu.add(exit);
 		exit.addActionListener(this);
+		
+		repaint();
+//		validate();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		if(e.getSource() == exit)
+		if (e.getSource() == exit)
 		{
 			System.exit(0);
 		}
+	}
+	
+	public Image loadImage(String ImageName)
+	{
+		return Toolkit.getDefaultToolkit().getImage(getClass().getResource(ImageName));
 	}
 
 }
