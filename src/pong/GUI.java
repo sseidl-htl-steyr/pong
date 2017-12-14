bf717c8d6d710d52d842a7877a06c709bb5acb6c
Bernd Berger <Bernd Berger@192.168.52.201>
2017-12-14 14:57:36 +0100

    Soweit funktionierender Startscreen

--- src/pong/GUI.java
+++ src/pong/GUI.java
@@ -3,13 +3,16 @@ package pong;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,8 +23,7 @@ public class GUI extends JFrame implements ActionListener
 	private CardLayout cl;
 	private JPanel mainMenu;
 	private JPanel canvas;
-    private PlayField pf;
-
+	private PlayField pf;
 
 	// Menübuttons
 	private JTextPane title;
@@ -34,29 +36,34 @@ public class GUI extends JFrame implements ActionListener
 	{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("BSS Pong");
-		setSize(800, 500);
+		setSize(1280, 720);
 		setResizable(false);
 
 		setIconImage(loadImage("Pong_Icon.png"));
-//		setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("Pong_Icon.png")));
-		
+		// setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("Pong_Icon.png")));
+
 		Container contentPane = getContentPane();
 		cl = new CardLayout();
 		contentPane.setLayout(cl);
 
-		mainMenu = new BackgroundPanel(loadImage("Pong.png"));
-		mainMenu.setLayout(new GridLayout(5, 1));
-//		mainMenu = new JPanel(new GridLayout(5, 1));
-//		mainMenu.setBackground(Color.GREEN);
+		mainMenu = new BackgroundPanel(loadImage("Pong_Test.png"));
+		// mainMenu = new BackgroundPanel(loadImage("Pong.png"));
+		mainMenu.setLayout(new GridLayout(1, 0));
+
+		Box vBox = Box.createVerticalBox();
+
+		// vBox.add(Box.createRigidArea(new Dimension(0, (int) (getHeight() / 1.5F +
+		// 0.5F)))); //Hoehe der Buttons veraendern
+
+		mainMenu.add(vBox);
 
 		canvas = new JPanel();
 		canvas.setBackground(Color.WHITE);
 
-        
-        pf = new PlayField(this);
-        canvas.setLayout(new BorderLayout());
-        canvas.add(pf, BorderLayout.CENTER);
-        contentPane.add(canvas);
+		pf = new PlayField(this);
+		canvas.setLayout(new BorderLayout());
+		canvas.add(pf, BorderLayout.CENTER);
+		contentPane.add(canvas);
 
 		contentPane.add(mainMenu);
 		contentPane.add(canvas);
@@ -66,45 +73,50 @@ public class GUI extends JFrame implements ActionListener
 
 		cl.show(contentPane, "main");
 
-		
 		title = new JTextPane();
 		title.setText("Pong");
 		title.setEditable(false);
-		mainMenu.add(title);
+		// mainMenu.add(title);
 
 		singleplayer = new JButton("Singleplayer");
+		singleplayer.setSize(getWidth() / 8, 50);
+		singleplayer.setMaximumSize(singleplayer.getSize());
 		singleplayer.setOpaque(true);
-		singleplayer.setContentAreaFilled(false);
+		// singleplayer.setContentAreaFilled(false);
 		singleplayer.setBorderPainted(false);
-		mainMenu.add(singleplayer);
 		singleplayer.addActionListener(this);
-
+		vBox.add(singleplayer);
 
 		multiplayer = new JButton("Multiplayer");
+		multiplayer.setSize(getWidth() / 8, 50);
+		multiplayer.setMaximumSize(singleplayer.getSize());
 		multiplayer.setOpaque(false);
-		multiplayer.setContentAreaFilled(false);
+		// multiplayer.setContentAreaFilled(false);
 		multiplayer.setBorderPainted(false);
-		mainMenu.add(multiplayer);
 		multiplayer.addActionListener(this);
-
+		vBox.add(multiplayer);
 
 		settings = new JButton("Settings");
+		settings.setSize(getWidth() / 8, 50);
+		settings.setMaximumSize(singleplayer.getSize());
 		settings.setOpaque(false);
-		settings.setContentAreaFilled(false);
+		// settings.setContentAreaFilled(false);
 		settings.setBorderPainted(false);
-		mainMenu.add(settings);
 		settings.addActionListener(this);
-
+		vBox.add(settings);
 
 		exit = new JButton("Exit");
-		exit.setOpaque(false);
-		exit.setContentAreaFilled(false);
+		exit.setSize(getWidth() / 8, 50);
+		exit.setMaximumSize(singleplayer.getSize());
+		// exit.setOpaque(false);
+		exit.setBackground(new Color(200, 0, 0));
+		// exit.setContentAreaFilled(false);
 		exit.setBorderPainted(false);
-		mainMenu.add(exit);
 		exit.addActionListener(this);
-		
+		vBox.add(exit);
+
 		repaint();
-//		validate();
+		// validate();
 	}
 
 	@Override
@@ -114,12 +126,12 @@ public class GUI extends JFrame implements ActionListener
 		{
 			System.exit(0);
 		}
-        if(e.getSource() == singleplayer)
-        {
-            cl.show(getContentPane(), "canvas");
-        }
+		if (e.getSource() == singleplayer)
+		{
+			cl.show(getContentPane(), "canvas");
+		}
 	}
-	
+
 	public Image loadImage(String ImageName)
 	{
 		return Toolkit.getDefaultToolkit().getImage(getClass().getResource(ImageName));
